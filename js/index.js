42d89560e76d74b6ad54dda6b4f4833772609836
Tony Thomas <tonythomas22@protonmail.com>
2017-07-12 13:30:33 -0700

    latest changes were not commited

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-$('.gallery').isotope({
-    itemSelector: '.photo',
-    //layoutMode: 'fitRows',
-    layoutMode: 'masonry',
-    masonry: {
-        columnWidth: 300,
-        gutter: 10,
-        fitWidth: true
-    }
+var $gallery = $('.gallery').isotope({
+  // options...
+  imageSelector: '.photo',
+  layoutMode: 'masonry'
 });
+// layout Isotope after each image loads
+$gallery.imagesLoaded().progress( function() {
+  $gallery.isotope('layout');
+});
+
+// masonry: {
+//     columnWidth: 300,
+//     gutter: 10,
+//     //fitWidth: true //does not work with percentage widths
+// }
+
+//layoutMode: 'fitRows',
